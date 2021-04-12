0327980055faa4c827d833df3d662ff4559514ff
huanglei <lei.huang@52ydwf.com>
2021-04-12 12:47:43 +0800

    <feat><apis>增加Test代码

---
+++ heima-leadnews-admin/src/main/java/com/heima/admin/service/Test.java
@@ -0,0 +1,13 @@
+package com.heima.admin.service;
+
+/**
+ * @Author： Huang Lei
+ * @Date： 2021/4/12 12:46
+ * @Version： 1.0
+ * @Description：
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hehe");
+    }
+}
