8d5bf380c1265f239300cac9b30269884ccf4d2c
huanglei <lei.huang@52ydwf.com>
2021-04-12 12:52:31 +0800

    <feat><apis>增加huanglei代码

---
+++ heima-leadnews-admin/src/main/java/com/heima/admin/controller/v1/huanglei.java
@@ -0,0 +1,13 @@
+package com.heima.admin.controller.v1;
+
+/**
+ * @Author： Huang Lei
+ * @Date： 2021/4/12 12:51
+ * @Version： 1.0
+ * @Description：
+ */
+public class huanglei {
+    public static void main(String[] args) {
+        System.out.println("qjkhakwjf");
+    }
+}
